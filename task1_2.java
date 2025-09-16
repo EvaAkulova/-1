43d244aa499af3819162fb7d48f8f73010adbc18
EvaAkulova <evaakulova7@gmail.com>
2025-09-16 08:45:56 +0300

    Add files via upload

---
+++ task1_2.java
@@ -0,0 +1,6 @@
+//создаем массив
+myList = [1,2,3,4,5]
+
+//создаем стек
+myList = []
+stack.push(1); stack.push(2);stack.pop();
---
+++ task1_3.cpp
@@ -0,0 +1,6 @@
+//Создаем массив
+int main()
+    //Создаем переменную myList
+    std ::vector<int> myList = {1,2,3,4,5,
+    return 0;
+}
